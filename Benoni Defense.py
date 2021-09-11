8a1a4fa5d2ea9fccd677aa361329c52dcbea069b
Nivesh-GitHub <89345698+Nivesh-GitHub@users.noreply.github.com>
2021-09-11 18:07:32 +0530

    Create Benoni Defense.py

---
+++ Benoni Defense.py
@@ -0,0 +1,14 @@
+#Benko Gambit
+board = chess.Board()
+board.push_san("d4")
+board.push_san("Nf6")
+board.push_san("c4")
+board.push_san("c5")
+board.push_san("d5")
+board.push_san("b5")
+
+
+
+
+
+board
